3b5f4805f67af1c683233cf8059c11c00ef30412
merillpr <merillpr16@gmail.com>
2023-11-11 11:05:19 +0700

    feat(product): CRUD product

---
+++ app/Traits/ResponseAPI.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Traits;
+
+trait ResponseAPI
+{
+    /**
+     * Core of response
+     * 
+     * @param   string          $message
+     * @param   integer         $statusCode  
+     * @param   boolean         $isSuccess
+     * @param   array|object    $data
+     */
+    public function coreResponse($message, $statusCode, $isSuccess = true, $data = null)
+    {
+        if(!$message) return response()->json(['message' => 'Message is required'], 500);
+        
+        if($isSuccess) {
+            return response()->json([
+                'message' => $message,
+                'success' => true,
+                'code' => $statusCode,
+                'results' => $data
+            ], $statusCode);
+        } else {
+            return response()->json([
+                'message' => $message,
+                'success' => false,
+                'code' => $statusCode,
+            ], $statusCode);
+        }
+    }
+
+    /**
+     * Send any success response
+     * 
+     * @param   string          $message
+     * @param   integer         $statusCode
+     * @param   array|object    $data
+     */
+    public function success($message, $data, $statusCode = 200, )
+    {
+        return $this->coreResponse(message: $message, statusCode: $statusCode, data: $data);
+    }
+
+    /**
+     * Send any error response
+     * 
+     * @param   string          $message
+     * @param   integer         $statusCode    
+     */
+    public function error($message, $statusCode = 500)
+    {
+        return $this->coreResponse(message: $message, statusCode: $statusCode, isSuccess: false);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Product
+Route::get('product', [ProductController::class, 'index']);
+Route::post('product', [ProductController::class, 'store']);
+Route::get('product/{id}', [ProductController::class, 'show']);
+Route::put('product/{id}', [ProductController::class, 'update']);
+Route::delete('product/{id}', [ProductController::class, 'destroy']);
+//End Product
