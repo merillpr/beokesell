996f58422dd755ab1292e3995a9d48886eb0add6
merillpr <merillpr16@gmail.com>
2023-11-21 12:23:47 +0700

    fix(transaction): add transaction rule

--- app/Http/Requests/Price/PriceRequest.php
+++ app/Http/Requests/Price/PriceRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests\Price;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class PriceRequest extends FormRequest
 {
